a7e33663224bc4867843fbb8c2b5aab200a692a3
Мошков Николай Евгеньевич <moshkovne@sibur.ru>
2022-10-30 18:49:49 +0300

    fix uppercase postgress, load DB pipeline improved

--- src/postamats/utils/prepare_data.py
+++ src/postamats/utils/prepare_data.py
@@ -12,6 +12,19 @@ import numpy as np
 import pandas as pd
 
 OBJECT_ID_COL = 'OBJECT_ID'
+# куда складывать промежуточные данные при подготовке табличек
+# содержимое папки добавлено в гитигнор, данные будут там появлять локально пи выполнении скриптов
+PREPARED_DATA_PATH = 'Notebooks/prepare_data/data'
+
+# названия промежуточных табличек с обработанными данными из
+# https://data.mos.ru/opendata/60562/data/table?versionNumber=3&releaseNumber=823
+# https://dom.gosuslugi.ru/#!/houses
+# которые используются для подготовки финальных табличек для заливки в базу
+PREPARED_GIS_FILE = 'prepare_gis_houses_data.pickle'
+PREPARED_DMR_FILE = 'prepare_dmr_houses_data.pickle'
+
+# как мы назовем колонку с координатами центроида полигона
+GEODATA_CENTER_COL = 'GEODATA_CENTER'
 
 
 def codes_to_str(codes_series: pd.Series) -> pd.Series:
@@ -88,19 +101,37 @@ def get_row_hashes(data: pd.DataFrame) -> pd.Series:
     return row.apply(get_text_hash)
 
 
-def create_geolist(geo_str):
+def create_geolist(geo_str: str) -> list:
+    """Превращает кривое представление геоданных полигонов
+    в лист с координатами [[x1, y1], [x2, y2], ...]
+    Используется при обработке данных из json "Адресный реестр объектов недвижимости города Москвы"
+    https://data.mos.ru/opendata/60562/data/table?versionNumber=3&releaseNumber=823
+
+    Args:
+        geo_str (str): _description_
+
+    Returns:
+        list: _description_
+    """
     geo_list = re.sub(r'[^0-9,.]', '', str(geo_str)).split(',')
     if geo_list != []:
-        geo_list = [[float(geo_list[i]), float(geo_list[i+1])] for i in range(0, len(geo_list)-1, 2)]
+        geo_list = [[float(geo_list[i]), float(geo_list[i+1])]
+                    for i in range(0, len(geo_list)-1, 2)]
     return geo_list
 
 
-def prepare_geo(geodata: str) -> str:
-    geodata = re.sub(r'[^\[\]0-9,.]', '', str(geodata))
-    return geodata.strip(',')
-
-
 def prepare_kad_num(kad_num: str) -> str:
+    """Превращает кривое представление кадастрового номера
+    в стандартный вид
+    Используется при обработке данных из json "Адресный реестр объектов недвижимости города Москвы"
+    https://data.mos.ru/opendata/60562/data/table?versionNumber=3&releaseNumber=823
+
+    Args:
+        kad_num (str): _description_
+
+    Returns:
+        str: _description_
+    """
     kad_num = re.sub(r'[^0-9\:]', '', str(kad_num))
     if (kad_num!='' and kad_num[0]==':'):
         return kad_num[1:]
@@ -108,7 +139,26 @@ def prepare_kad_num(kad_num: str) -> str:
 
 
 def calc_polygon_centroid(coords: List[List[float]]) -> List[float]:
-    # Пример coords = ((0., 0.), (0., 1.), (1., 1.), (1., 0.), (0., 0.))
+    """Вычисляет координаты цетроида полигона
+     В данных вместо полигонов встречаются точки и линии, функция их тоже обрабатывает
+
+    Args:
+        coords (List[List[float]]): координаты в формате
+         [[x1, y1], [x2, y2], [x3, y3], ...]
+
+    Returns:
+        List[float]: координаты центроида в формате [x_c, y_c]
+    """
+    # для тестирования:
+    # print(calc_polygon_centroid([[0,0], [1,0], [1,1], [0,1]]),
+    #       'expect: [.5, .5]')
+    # print(calc_polygon_centroid([[0,1], [0,-1], [2,0]]),
+    #       'expect: [2/3, 0]')
+    # print(calc_polygon_centroid([[0,-1], [0,1], [2,0]]),
+    #       'expect: [2/3, 0]')
+    # print(calc_polygon_centroid([[0,0], [1,0], [1.5,0.5], [1,1], [0,1], [-.5,.5]]),
+    #       'expect: [.5, .5]')
+
     if not isinstance(coords, list):
         warn('coords is not list')
         return np.nan
