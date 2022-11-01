e8871193b09d9a1b977dbb00aace0946cb710706
Kuznetsov Sergey <46071230+SerSmith@users.noreply.github.com>
2022-11-01 22:05:19 +0300

    add get_all_points

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from typing import Union
+import os
+
+from postamats import connections
 
 from fastapi import FastAPI
 
@@ -9,3 +12,13 @@ app = FastAPI()
 def get_possible_postomat_places():
 
     return {"Hello": "World"}
+
+
+@app.get("/get_all_postomat_places")
+def get_all_postomat_places():
+
+    db = connections.DB()
+
+    all_postamat_places = db.get_table_from_bd("all_objects_data")
+
+    return all_postamat_places.to_dict(orient="records")
