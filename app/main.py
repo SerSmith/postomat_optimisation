e8cb8c126b5714fb8d0f276eb0a51dcbfe90e9df
Mila601 <mila_601@mail.ru>
2022-11-02 22:26:43 +0300

    add filter in get_all_postimat_places

--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ def get_all_postomat_places():
 
     db = connections.DB()
 
-    all_postamat_places = db.get_table_from_bd("all_objects_data")
+    #all_postamat_places = db.get_table_from_bd("all_objects_data")
+    all_postamat_places = db.get_by_sql("select * from all_objects_data where object_type!='многоквартирный дом' ")
 
     return all_postamat_places.to_dict(orient="records")
