fe9f83f52d9afc3a8cb8d574cb7665faee7d3fe4
Mila601 <mila_601@mail.ru>
2022-11-04 15:12:08 +0300

    add CORSM

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
+import uvicorn
 from typing import Union
 import os
 
 from postamats.utils import connections
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/say_hi")
 def get_possible_postomat_places():
@@ -24,13 +33,13 @@ def get_all_postomat_places():
 
     return all_postamat_places.to_json(orient='records', force_ascii=False)
 
- def calculate_workload(center_mass_pd, distance_matrix_pd):
+def calculate_workload(center_mass_pd, distance_matrix_pd):
     
     distance_matrix_pd['walk_time'] = distance_matrix_pd['distance']
 
     only_nearest_points_min_dist = distance_matrix_pd.loc[distance_matrix_pd.groupby('id_center_mass').walk_time.idxmin()]
 
-    only_nearest_points_min_dist_with_pop = only_nearest_points_min_dist.merge(center_mass_pd, on='id_center_mass')
+    only_nearest_points_min_dist_with_pop = only_nearest_popipints_min_dist.merge(center_mass_pd, on='id_center_mass')
 
     quantity_people_to_postomat = only_nearest_points_min_dist_with_pop.groupby('object_id').agg({'population': 'sum'}).reset_index()
 
@@ -41,10 +50,13 @@ def get_all_postomat_places():
 
 @app.get("/get_point_statistics")
 def find_heat_map(step, object_id_str):
-    
+
     db = connections.DB()
     #object_id_str = ["'" + str(s) + "'" for s in all_postamat_places.object_id[:2000]]
     distance_matrix_filter= db.get_by_filter("distances_matrix_filter", {"step": [step], "object_id": object_id_str})
     center_mass_pd = db.get_table_from_bd('centers_mass')
     quantity_people_to_postomat, distance_till_nearest_postomat = calculate_workload(center_mass_pd, distance_matrix_filter)
     return quantity_people_to_postomat.to_json(orient='records'), distance_till_nearest_postomat.to_json(orient='records')   
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
