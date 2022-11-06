a544589b1006a72d5c9938867783c5ba84067f8e
Kuznetsov Sergey <46071230+SerSmith@users.noreply.github.com>
2022-11-06 15:38:25 +0300

    Update main.py

--- apps/cluster-optimizer/main.py
+++ apps/cluster-optimizer/main.py
@@ -1,7 +1,11 @@
 import uvicorn
 
-from fastapi import FastAPI, Query
+import json
+from typing import List
+from fastapi import FastAPI, Query, Depends
 from fastapi.middleware.cors import CORSMiddleware
+import postamats.utils.helpers as h
+from postamats.optimization import classtopt
 
 
 app = FastAPI()
@@ -13,6 +17,39 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/get_kmeans_optimize_points")
+def get_kmeans_optimize_points(object_type_filter_list: List[str] = Depends(h.parse_object_type_filter_list),
+                                  district_type_filter_list: List[str] = Depends(h.parse_district_type_filter_list),
+                                  adm_areat_type_filter_list: List[str] = Depends(h.parse_adm_areat_type_filter_list),
+                                  fixed_points: List[str] = Depends(h.parse_list_fixed_points),
+                                  banned_points: List[str] = Depends(h.parse_banned_points_list),
+                                  quantity_postamats_to_place:int =Query(1500, description="Количество постаматов "),
+                                  metro_weight:float =0.5,
+                                  large_houses_priority:int =0.5,
+                                  max_time:int =15):
+
+    possible_postomats = h.make_points_lists(db,
+                                            object_type_filter_list,
+                                            district_type_filter_list,
+                                            adm_areat_type_filter_list,
+                                            banned_points)
+
+    if fixed_points is None:
+        fixed_points = []
+
+    optimised_list = classtopt.kmeans_optimize_points(possible_postomats,
+                                                      fixed_points,
+                                                      quantity_postamats_to_place,
+                                                      metro_weight,
+                                                      large_houses_priority,
+                                                      max_time)
+
+
+    output = json.dumps({'optimized_points': optimised_list})
+
+    return output
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
