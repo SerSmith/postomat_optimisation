e4b17bbf315ad88e0bb60fed6f2d09b19a6af72d
Kuznetsov Sergey <46071230+SerSmith@users.noreply.github.com>
2022-11-06 18:28:25 +0300

    upd opt

--- apps/solver-optimiser/main.py
+++ apps/solver-optimiser/main.py
@@ -32,18 +32,18 @@ def get_optimized_postomat_places(object_type_filter_list: List[str] = Depends(h
                                   adm_areat_type_filter_list: List[str] = Depends(h.parse_adm_areat_type_filter_list),
                                   fixed_points: List[str] = Depends(h.parse_list_fixed_points),
                                   banned_points: List[str] = Depends(h.parse_banned_points_list),
-                                  quantity_postamats_to_place:int =Query(1500, description="Количество постаматов "),
-                                  step:float =0.5,
-                                  metro_weight:float =0.5,
-                                  opt_time:int =250,
-                                  max_time:int =15):
+                                  quantity_postamats_to_place:int =Query(1500, description="Количество постаматов, которыое необходимо поставить. (шт)"),
+                                  step:float =Query(0.1, description="Размер клетки при агрегации (км)"),
+                                  metro_weight:float =Query(0.5, description="Важность близости к мето по сравнению с близостью к домам. От 0(учитывать только дома) до 1(учитывать только станции метро). (безоазмерный)"),
+                                  opt_time:int =Query(200, description="Максимальное время работы солвера, если за это время не удалось найти решение отличающееся от потенциально возможного оптимума менее чем на 0.01% то выдасться лучшее имеющееся решение. (сек)"),
+                                  max_time:int =Query(15, description="Максимальное время, за которое человек готов дойти до постомата. Если человеку идти дольше, чем этот параметр, то мы считаем, что он не будет ходить в этот постамат. (мин)")):
 
     #  Зададим параметры для непосредственно солвера, полный список можно найти тут
     #  ratioGap - допустимое отклонение от потенциального оптимального решения
     #  sec - максимальное время работы солвераб после которого он выдадет ссамое  лучшее найденное решенее
     #  Если будет обнаружено, что имеющееся решениее отличается от потенциального оптимального меньше чем на ratioGap, 
     #  то солвер выдаст результат раньше
-    kwargs = {'ratioGap': 0.00001, 'sec': opt_time}
+    kwargs = {'ratioGap': 0.0001, 'sec': opt_time}
 
     # Конектктимся к базе, данные берутся из переменных окружения(не было времени разбираться  ссекретницей клауда)
     db = connections.DB()
