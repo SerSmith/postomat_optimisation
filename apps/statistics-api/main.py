243e57a01edc374c75ad28259f4fb769625df90c
Mila601 <mila_601@mail.ru>
2022-11-06 18:25:21 +0300

    added get_excel

--- apps/statistics-api/main.py
+++ apps/statistics-api/main.py
@@ -10,6 +10,9 @@ from postamats.utils.helpers import parse_list_fixed_points, parse_list_possidbl
 
 from fastapi import FastAPI, Query, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+from slugify import slugify
+import io
 
 app = FastAPI()
 
@@ -56,6 +59,8 @@ def get_optimized_postomat_places(possible_points: List[str] = Depends(parse_lis
     #return fixed_points
 
 
+
+
 @app.get("/get_point_statistics")
 def find_heat_map(step: float = 1, walk_time: float = 15, object_id_str: List[str] = Depends(parse_list_object_id)):
 
@@ -76,5 +81,60 @@ def find_heat_map(step: float = 1, walk_time: float = 15, object_id_str: List[st
     list_for_json_percent = [{"time" : list_time_m[0], "percent_people" : list_result_percent[0]},{"time":list_time_m[1], "percent_people": list_result_percent[1]}, {"time":list_time_m[2], "percent_people": list_result_percent[2]}, {"time":list_time_m[3],"percent_people":list_result_percent[3]}]
     return quantity_people_to_postomat.to_json(orient='records'), distance_till_nearest_postomat.to_json(orient='records') , json.dumps(list_for_json_percent)  
 
+@app.get("/get_excel")
+def get_excel(method_name: str, postomat_points: List[str] = Depends(parse_list_object_id),  walk_time: float = 15, step: float = 0.5):
+
+    db = connections.DB()
+    walk_time = walk_time*60
+    #postomat_points_str = ["'" + s + "'" for s in postomat_points ]
+    center_mass_pd = db.get_by_filter('centers_mass', filter_dict = {"step":[step]})
+    distance_matrix_filter= db.get_by_filter("distances_matrix_filter", filter_dict = {"step": [step], "object_id": postomat_points}, additional_filter=f' walk_time<={walk_time}')
+    print('read all table')
+
+    #postomat_points_str = ["'" + s + "'" for s in postomat_points ]
+    postamat_places = db.get_by_filter("all_objects_data", {"object_id": postomat_points} )
+    postamat_places['Модель расчета'] = method_name
+
+    postamat_places = postamat_places.reset_index()
+    quantity_people_to_postomat, _ = calculate_workload(center_mass_pd, distance_matrix_filter)
+
+    postamat_places = postamat_places.merge(quantity_people_to_postomat, on="object_id", how='left')
+    postamat_places.population = postamat_places.population.fillna(0)
+    postamat_places.population = postamat_places.population.astype(int)
+
+    COLUMNS_RENAME = {
+        'index': 'No п/п (номер по порядку)',
+        'adm_area': 'Административный округ',
+        'district': 'Район',
+        'object_type': 'Тип объекта размещения',
+        'lat': 'Координаты широта',
+        'lon': 'Координаты долгота',
+        'address': 'Адрес точки размещения',
+        'population': 'Количество людей, для которых этот постомат ближайший',
+    }
+    postamat_places = postamat_places.rename(COLUMNS_RENAME, axis=1)
+
+    return_data = postamat_places[['No п/п (номер по порядку)',\
+                        'Административный округ',\
+                        'Район',\
+                        'Тип объекта размещения',\
+                        'Координаты широта',\
+                        'Координаты долгота',\
+                        'Адрес точки размещения',\
+                        'Модель расчета',
+                        'Количество людей, для которых этот постомат ближайший']]
+    output = io.BytesIO()
+
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+
+    #file_name = slugify(place['result']['name'])
+        
+    result = return_data.to_excel(writer, encoding='cp1251', index=False)
+    writer.save()
+    xlsx_data = output.getvalue()
+
+    return StreamingResponse(io.BytesIO(xlsx_data), media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
